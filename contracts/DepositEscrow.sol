57d6c8033619d11fafce9040887840cab8a8a630
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-09-13 23:55:57 +1000

    feat: init DepositEscrow contract

---
+++ contracts/DepositEscrow.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DepositEscrow is Ownable {
+
+    // Deposit function with nickname
+
+    // Admin withdraw function to release all deposits
+
+    // Safe Smart Account with multisig on using funds.
+        // Able to use Gnosis Pay Card
+
+}
